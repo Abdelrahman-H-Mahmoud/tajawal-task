500885e8728eae84aa90a1de797b176f81286552
wahdan01 <a_wahdan93@hotmail.com>
2018-05-15 07:27:18 +0200

    fetch hotels (data) from api

---
+++ src/api/hotels.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const axios = require('axios');
+
+const api = require('./../config/api');
+
+const fetchHotels = async (request, reply)=>{
+    let hotels = await axios({
+        method:'get',
+        url:api.url
+    }).then(function (response) {
+        return response;
+    }).catch(function (error) {
+        return error;
+    });      
+    return hotels.data;  
+};
+module.exports = {
+    fetchHotels
+};
