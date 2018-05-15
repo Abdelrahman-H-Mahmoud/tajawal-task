7de1c475e834cbf73188a323e532b5e4bc762b66
wahdan01 <a_wahdan93@hotmail.com>
2018-05-15 07:27:36 +0200

    added api config

---
+++ src/config/api.js
@@ -0,0 +1,7 @@
+module.exports = function() {    
+    return {
+        url : 'https://api.myjson.com/bins/tl0bp',
+        searchCriteria:['name','city','price','date'],
+        sortCriteria:['sort_by']
+    };
+}();
