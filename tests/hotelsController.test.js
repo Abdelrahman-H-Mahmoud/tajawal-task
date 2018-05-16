e5727276757cfc05f8faee644a82a40a929d91df
wahdan01 <a_wahdan93@hotmail.com>
2018-05-16 04:51:44 +0200

    INP hotel testing

---
+++ tests/hotelsController.test.js
@@ -0,0 +1,34 @@
+const {expect} = require('code') ,
+    Lab = require('lab') ,     
+    lab = exports.lab = Lab.script(),
+    server = require('../server.js'),
+    hotelData = require('./hotelData');
+
+lab.experiment('Filter Hotels', function() {
+        
+    // lab.test('fetch all hotels', async function() {
+    //     const options = {
+    //         method: 'GET',
+    //         url: '/hotels'
+    //     };
+    //     const response = await server.inject(options);
+    //     expect(response.statusCode).to.equal(200);
+    //     await server.stop();
+    // });
+
+    lab.test('Get hotel details by name', async function() {
+
+    });
+
+    lab.test('Get hotel details by city', async function() {
+
+    });
+
+    lab.test('Get hotel details by price range', async function() {
+
+    });
+
+    lab.test('Get hotel details by date range', async function() {
+
+    });
+});
