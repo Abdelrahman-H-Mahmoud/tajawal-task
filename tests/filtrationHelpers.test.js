7199442e11f70bc5c34bb5ab38bf84ce1907eb84
AbdelRahman Wahdan <a.wahdan@icloudit.com>
2018-05-16 12:51:00 +0200

    INP : testing filtration helper method

---
+++ tests/filtrationHelpers.test.js
@@ -0,0 +1,16 @@
+// const {expect} = require('code') ,
+const Lab = require('lab') ,     
+    lab = exports.lab = Lab.script();
+    // hotelData = require('./hotelData'),
+    // hotelsService = require('./../src/services/hotels');
+    //  {filterByString} = require('../src/helpers/filtrationHelpers');
+
+
+   
+lab.experiment('Filter Hotels by (string) search key', function() {
+        
+    lab.test('filter hotels by (string) key ', function() {
+
+    });
+   
+});
