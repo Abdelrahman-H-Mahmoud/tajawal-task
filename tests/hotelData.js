a001a3a476c54d0afb7a73397066a838cb6ad657
wahdan01 <a_wahdan93@hotmail.com>
2018-05-16 04:50:55 +0200

    added fake hotel data for testing

---
+++ tests/hotelData.js
@@ -0,0 +1,60 @@
+module.exports=[ 
+    {
+        name: 'Concorde Hotel',
+        price: 79.4,
+        city: 'Manila',
+        availability: [
+            {
+                from: '10-10-2020',
+                to: '19-10-2020'
+            },
+            {
+                from: '22-10-2020',
+                to: '22-11-2020'
+            },
+            {
+                from: '03-12-2020',
+                to: '20-12-2020'
+            }
+        ]
+    },
+    {
+        name: 'Golden Tulip',
+        price: 109.6,
+        city: 'paris',
+        availability: [
+            {
+                from: '04-10-2020',
+                to: '17-10-2020'
+            },
+            {
+                from: '16-10-2020',
+                to: '11-11-2020'
+            },
+            {
+                from: '01-12-2020',
+                to: '09-12-2020'
+            }
+        ]
+    },
+    {
+        name: 'Media One Hotel',
+        price: 102.2,
+        city: 'dubai',
+        availability: [
+            {
+                from: '10-10-2020',
+                to: '15-10-2020'
+            },
+            {
+                from: '25-10-2020',
+                to: '15-11-2020'
+            },
+            {
+                from: '10-12-2020',
+                to: '15-12-2020'
+            }
+        ]
+    }
+      
+];
