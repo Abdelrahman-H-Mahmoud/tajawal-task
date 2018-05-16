fbff2a4382520651896d82195abbae4dfa853827
wahdan01 <a_wahdan93@hotmail.com>
2018-05-16 04:49:48 +0200

    moved searchCriteria from api to hotel model

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,5 @@
 module.exports = function() {    
     return {
-        url : 'https://api.myjson.com/bins/tl0bp',
-        searchCriteria:['name','city','price','date'],
-        sortCriteria:['sort_by']
+        url : 'https://api.myjson.com/bins/tl0bp'
     };
 }();
---
+++ src/models/hotel.js
@@ -0,0 +1,8 @@
+
+
+const searchCriteria =['name','city','price','date'];
+const sortCriteria =['sort_by'];
+module.exports ={
+    searchCriteria,
+    sortCriteria
+};
