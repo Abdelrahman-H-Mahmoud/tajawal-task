2a3c5c7184dc146f3a20eed2ca84fce2a6da91da
wahdan01 <a_wahdan93@hotmail.com>
2018-05-16 04:51:27 +0200

    added sort and filtration

--- src/controllers/hotelController.js
+++ src/controllers/hotelController.js
@@ -1,22 +1,26 @@
 'use strict';
 const _ = require('lodash'),
     hotelsApi = require('../services/hotels'),
-    apiConfig = require('../config/api');
+    hotel = require('../models/hotel');
     
 const fetchHotels = (request)=>{
-    let searchCriteria = _.pick(request.query,apiConfig.searchCriteria);
-    let sortCriteria = _.pick(request.query,apiConfig.sortCriteria);
+    let searchCriteria = _.pick(request.query,hotel.searchCriteria);
+    let sortCriteria = _.pick(request.query,hotel.sortCriteria);
+    let hotels;
 
     return hotelsApi.fetchHotels()
         .then((result)=>{
+            hotels = result;
             if(_.isEmpty(searchCriteria) && _.isEmpty(sortCriteria)){
-                return result; 
+                return hotels; 
             }
-            console.log(result.hotels[0].price,searchCriteria);
-            let hotels = _.filter(result.hotels, searchCriteria);
-
+            if(!_.isEmpty(searchCriteria)){
+                hotels = hotelsApi.filterHotels(result.hotels, searchCriteria);
+            }
+            if(!_.isEmpty(sortCriteria) && !_.isEmpty(hotels)){
+                hotels = hotelsApi.sortHotels(hotels,sortCriteria['sort_by']);
+            }
             return hotels;
-            
         })
         .catch((error)=>{
             return error;
--- src/services/hotels.js
+++ src/services/hotels.js
@@ -1,8 +1,8 @@
 'use strict';
 
-const axios = require('axios');
-
-const api = require('./../config/api');
+const axios = require('axios'),
+    api = require('./../config/api'),
+    _ = require('lodash');
 
 const fetchHotels = async ()=>{
     let hotels = await axios({
@@ -15,6 +15,53 @@ const fetchHotels = async ()=>{
     });      
     return hotels.data;  
 };
+
+const filterHotels = (data , criterias)=>{
+    let result;
+    if(criterias.hasOwnProperty('name')){
+        result = _.filter(data,(element)=>{
+            return element['name'].toLowerCase() === criterias['name'].toLowerCase();
+        });
+    }
+    if(criterias.hasOwnProperty('price')){
+        let priceRange = criterias['price'].split(':').map(element => {
+            return parseFloat (element);
+        });
+        result = _.filter(data,function(element){
+            return element['price'] >= priceRange[0] && element['price'] <= priceRange[1]; 
+        });
+    }
+    if(criterias.hasOwnProperty('city')){
+        result = _.filter(data,(element)=>{
+            return element['city'].toLowerCase() === criterias['city'].toLowerCase();
+        });
+    }
+    if(criterias.hasOwnProperty('date')){
+        // let availabilityRange = criterias[criteria].split(':').map(element => {
+        //     let [day, month, year] = element.split('-');
+        //     let date = `${month}-${day}-${year}`;
+        //     return Date.parse(date);
+        // });
+        // result = _.filter(data,function(element){
+        //     console.log(element['availability']);
+        //     return element[criteria] >= availabilityRange[0] && element[criteria] <= availabilityRange[1]; 
+        // });
+    }
+
+ 
+
+    return result;
+
+};
+
+const sortHotels =  (data,sortCriteria)=>{
+    if(data.hasOwnProperty('hotels')){
+        data = data.hotels;
+    }
+    return  _.sortBy(data,sortCriteria);
+};
 module.exports = {
-    fetchHotels
+    fetchHotels,
+    filterHotels,
+    sortHotels
 };
