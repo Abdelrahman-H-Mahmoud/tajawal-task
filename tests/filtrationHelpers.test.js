5ed77e92890adcfc0b50970f1cc406f0265cc730
AbdelRahman Wahdan <a.wahdan@icloudit.com>
2018-05-17 16:05:20 +0200

    add price and date helper functions test

--- tests/filtrationHelpers.test.js
+++ tests/filtrationHelpers.test.js
@@ -2,7 +2,7 @@ const {expect} = require('code') ,
     Lab = require('lab') ,     
     lab = exports.lab = Lab.script(),
     hotelData = require('./hotelData'),
-    {filterByString} = require('../src/helpers/filtrationHelpers');
+    {filterByString,filterByPrice,filterByDate,parseStringDateToDate} = require('../src/helpers/filtrationHelpers');
 
 
    
@@ -15,5 +15,32 @@ lab.experiment('Filter Hotels by (string) search key', function() {
         
         expect(result[0]['city']).to.equal(hotelCity['city']);
     });
+
+    lab.test('filter hotels by (price) key ', function() {
+
+        let hotelPriceRange = '20:103';
+
+        let priceRange = hotelPriceRange.split(':').map(element => {
+            return parseFloat (element);
+        });
+        let result = filterByPrice(hotelData,hotelPriceRange);
+        expect(priceRange[0]<=result[0]['price']<=priceRange[1]).to.equal(true);
+
+    });
+
+    lab.test('filter hotels by (date) key ', function() {
+
+        let hotelDateRange = '10-10-2020:17-10-2020';
+
+        let result = filterByDate(hotelData,hotelDateRange);
+
+        hotelDateRange = hotelDateRange.split(':').map(element => {
+            return parseStringDateToDate(element);
+        });
+        let from = parseStringDateToDate(result[0]['availability'][0]['from']);
+        expect(from >=hotelDateRange[0]).to.equal(true);
+
+    });
+    
    
 });
