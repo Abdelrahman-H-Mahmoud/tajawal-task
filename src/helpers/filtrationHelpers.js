e2b4c1381e746553debceed66c5c70343734ee34
AbdelRahman Wahdan <a.wahdan@icloudit.com>
2018-05-17 16:05:40 +0200

    add filter by date helper function

--- src/helpers/filtrationHelpers.js
+++ src/helpers/filtrationHelpers.js
@@ -2,8 +2,31 @@ const _ = require('lodash');
 
 
 const filterByString = (data ,searchKey,searchValue)=>{
-    return _.filter(data,(element)=>{
-        return element[searchKey].toLowerCase() === searchValue.toLowerCase();
+    return _.filter(data,(hotel)=>{
+        return hotel[searchKey].toLowerCase() === searchValue.toLowerCase();
+    });
+};
+
+const filterByPrice = (data ,priceRangeStr)=>{
+    let priceRange = priceRangeStr.split(':').map(element => {
+        return parseFloat (element);
+    });
+    return _.filter(data,function(hotel){
+        return hotel['price'] >= priceRange[0] && hotel['price'] <= priceRange[1]; 
+    });
+};
+
+const filterByDate = (data ,dateStr)=>{
+    let availabilityRange = dateStr.split(':').map(element => {
+        return parseStringDateToDate(element);
+    });
+    return data.filter(hotel => {
+        hotel['availability'] = hotel['availability'].filter(range => {
+            let from = parseStringDateToDate(range['from']);
+            let to = parseStringDateToDate(range['to']);
+            return from <= availabilityRange[0] && to >= availabilityRange[1];
+        });
+        return hotel['availability'].length > 0;
     });
 };
 
@@ -14,5 +37,7 @@ const parseStringDateToDate = (str)=>{
 
 module.exports={
     filterByString,
-    parseStringDateToDate
+    parseStringDateToDate,
+    filterByPrice,
+    filterByDate
 };
